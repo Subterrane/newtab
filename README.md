432dba3c32a5d9015df186fc72e1faacd4c7f134
Will Munslow <will@subterrane.com>
2020-01-15 14:12:52 -0800

    Add control to change time zone

--- README.md
+++ README.md
@@ -11,12 +11,6 @@ Custom browser "new tab" page
 
 [Clone or Download](https://help.github.com/en/github/creating-cloning-and-archiving-repositories/cloning-a-repository) to a folder on your computer and update your browser settings. See below for an example in Safari.
 
-## TODO
-
-- set timezone
-
-  For now, hack `time.js` with your [timezone](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones).
-
 ## Install Example - Safari
 
 ![Safair setup](safari.png)
--- time.js
+++ time.js
@@ -1,17 +1,31 @@
-const displayTime = () =>
-  (document.getElementById("time").innerHTML = new Date().toLocaleTimeString(
-    "en-US",
-    {
-      timeZone: "America/Los_Angeles",
-      weekday: "long",
-      year: "numeric",
-      month: "long",
-      day: "2-digit",
-      hour: "numeric",
-      minute: "2-digit",
-      timeZoneName: "short"
-    }
-  ));
+const getTimeZone = () =>
+  localStorage.getItem("timeZone") || "America/Los_Angeles";
+const setTimeZone = tz => localStorage.setItem("timeZone", tz);
 
+const displayTime = () => {
+  try {
+    document.getElementById("time").innerHTML = new Date().toLocaleTimeString(
+      "en-US",
+      {
+        timeZone: getTimeZone(),
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "2-digit",
+        hour: "numeric",
+        minute: "2-digit",
+        timeZoneName: "short"
+      }
+    );
+  } catch (e) {
+    console.warn(`${e.message} setting to "America/Los_Angeles". See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones`);
+    setTimeZone("America/Los_Angeles");
+  }
+};
 setInterval(() => displayTime(), 10 * 1000);
 displayTime();
+
+document.getElementById("time_zone").addEventListener("click", () => {
+  setTimeZone(prompt("New Time Zone", getTimeZone()));
+  displayTime();
+});
