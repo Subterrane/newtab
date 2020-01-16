d97d2627f7af2c6261352f8e8afff0f141a0d5e6
Will Munslow <will@subterrane.com>
2020-01-16 00:28:43 -0800

    update command

--- terminal/command.sh
+++ terminal/command.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cwd="$(cd "$(dirname "$0")"; pwd)"
 echo "const output=\`" > $cwd/output.js
-/usr/local/bin/docker service ls --format "table {{.Name}}\t{{.Replicas}}" >> $cwd/output.js
+ls -al >> $cwd/output.js
 echo "\`;" >> $cwd/output.js
 echo "document.getElementById(\"terminal\").innerText = output.trim();" >> $cwd/output.js
--- terminal/output.js
+++ terminal/output.js
@@ -1,24 +1,16 @@
 const output=`
-NAME                        REPLICAS
-ol_access-service           1/1
-ol_apps-service             1/1
-ol_core-api                 1/1
-ol_form-service             1/1
-ol_haproxy-flow             1/1
-ol_haproxy-swarm-listener   1/1
-ol_mfa-service              1/1
-ol_monorail-admin           1/1
-ol_monorail-portal          1/1
-ol_monorail-ui              1/1
-ol_onelogin-fed             1/1
-ol_onelogin-profile         1/1
-ol_postgres                 1/1
-ol_rabbitmq                 1/1
-ol_redis-server             1/1
-ol_traefik                  1/1 (max 1 per node)
-ol_web-admin                1/1
-ol_web-login                1/1
-ol_web-portal               1/1
-ol_web-profile              1/1
+total 2536
+drwxr-xr-x  12 will  staff     384 Jan 15 18:44 .
+drwxr-xr-x  15 will  staff     480 Jan 14 20:29 ..
+drwxr-xr-x  16 will  staff     512 Jan 15 18:44 .git
+-rw-r--r--   1 will  staff     504 Jan 15 18:44 README.md
+-rw-r--r--   1 will  staff     287 Jan 14 23:22 background.js
+-rw-r--r--   1 will  staff  713173 Jan 15 18:44 example.png
+-rw-r--r--   1 will  staff    1475 Jan 15 18:44 links.js
+-rw-r--r--   1 will  staff  556428 Jan 14 23:30 safari.png
+-rw-r--r--   1 will  staff    1604 Jan 15 18:44 style.css
+-rw-r--r--   1 will  staff     833 Jan 15 18:44 tab.html
+drwxr-xr-x   6 will  staff     192 Jan 15 18:44 terminal
+-rw-r--r--   1 will  staff     921 Jan 15 18:44 time.js
 `;
 document.getElementById("terminal").innerText = output.trim();
